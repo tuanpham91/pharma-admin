a1fab0a975791fdb73a759779d6e54715c8fbae4
Anh Tuan Pham <phamtuanrk1991@gmail.com>
2019-08-07 22:29:34 +0200

    Update, UI goes with electron

--- src/main.go
+++ src/main.go
@@ -5,23 +5,14 @@ import (
 	_ "util"
 	_ "time"
 	"recordType"
-	"dbconnector"
-	"database/sql"
 )
 
 var pathToDatabase = "./record.db"
 
 func main() {
-	dbconnector.Database, _ = sql.Open("sqlite3", pathToDatabase)
+
 	fmt.Println("Welcome to Smart Pharma")
-	// st := int64(time.Now().Unix())
-	dbconnector.GenerateDatabases()
-	// rec := createARecord("tuan",10,99.99,"1.1.2010", "2.2.2020")
-	// dbconnector.AddRecordToDatabase(rec)
-	// fmt.Println(util.ConvertToFormattedDate(st))
-	//rows := dbconnector.SelectQueryFromDB("Select * From med_record")
-	//res := dbconnector.RowsToRecord(rows)
-	//fmt.Println(res)
+	// Electron here we go
 }
 
 func createARecord(name string, quantity int, price float32, expirationDate string, dateOfRecord string) recordType.Record {
