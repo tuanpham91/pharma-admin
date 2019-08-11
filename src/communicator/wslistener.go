f9099c161c57dfb7a30d461821e42d2779424b51
Anh Tuan Pham <phamtuanrk1991@gmail.com>
2019-08-11 13:19:56 +0200

    Add some endpoints

--- src/communicator/wslistener.go
+++ src/communicator/wslistener.go
@@ -1,10 +1,12 @@
 package communicator
-
+// security is not required, because local.
 import (
 	"net/http"
 	"fmt"
 	"recordType"
 	"encoding/json"
+	"strings"
+	"dbconnector"
 )
 
 var (
@@ -19,7 +21,8 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func setUpRoutes() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/record",getRecordRoute )
+	http.HandleFunc("/record", getRecordRoute)
+	http.HandleFunc(addRecord, addRecordRequestRoute)
 }
 
 func getRecordRoute(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +30,34 @@ func getRecordRoute(w http.ResponseWriter, r *http.Request) {
 	testRecord := recordType.ItemInventory{1, "Test", 1, "1.1.2020"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(testRecord)
+}
+
+// To add a new record to database, must be POST
+func addRecordRequestRoute(w http.ResponseWriter, r *http.Request) {
+	//Check if content is JSON 
+	if (r.Method == "GET") {
+		http.Error(w, "This type of Request is not allowed", http.StatusBadRequest)
+		return
+	} 
+	if (!strings.Contains(r.Header.Get("Content-Type"),"json")){
+		http.Error(w, "A json format is required", http.StatusBadRequest)
+		return
+	}	
+	//Read arguments from request
+	decoder := json.NewDecoder(r.Body)
+	var record recordType.Record
+	err := decoder.Decode(&record)
+	if (err != nil) {
+		http.Error(w, "An internal error occurred", http.StatusInternalServerError)
+		return
+	}
+	dbconnector.HandleRecord(record)
+	w.WriteHeader(http.StatusOK)
+}
+
+// There should be some criterias to filter 
+func getRecordsWithQuery(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func StartWebserver() {
